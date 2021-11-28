b9238c8be59d21ce221044d35516090ab71cb45f
abirbenkhaled <abouurabenkhaled@gmail.com>
2021-11-28 14:07:03 +0100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RoutingappModule } from './routingapp.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingappModule
   ],
   providers: [],
   bootstrap: [AppComponent]
