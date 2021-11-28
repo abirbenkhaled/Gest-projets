15cac059ed06294c0fb38b8e2575d12da6148c13
nourhenekhemiri <nourhenekhmiri46@gmail.com>
2021-11-28 16:12:38 +0100

    v

--- src/app/routingapp.module.ts
+++ src/app/routingapp.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProjetComponent } from './projet/projet.component';
-<<<<<<< HEAD
+
 import { ProfilComponent } from './profil/profil.component';
-=======
+
 import { WorkspaceComponent } from './workspace/workspace.component';
->>>>>>> 657e3f754cd76e383764f39ae834be795c0aa67e
+
 
 
 const routes:Routes = [
   {path:'projet',component:ProjetComponent},
-<<<<<<< HEAD
-  {path:'profil',component:ProjetComponent}
-];
-=======
+
+  {path:'profil',component:ProjetComponent},
+
+
   {path:'workspace', component:WorkspaceComponent}
-]
->>>>>>> 657e3f754cd76e383764f39ae834be795c0aa67e
+];
+
 
   
   
