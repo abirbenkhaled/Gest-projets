954dac0678fb63b4fb39838a1dcdef0ee6bc4785
abirbenkhaled <abouurabenkhaled@gmail.com>
2022-01-02 01:12:52 +0100

    modification projet

--- src/app/projet/projet.component.ts
+++ src/app/projet/projet.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjetComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  data1:any
+  data:any;
+  token: any;
+  user :any;
+  public projects :any
   ngOnInit(): void {
+    this.token = localStorage.getItem('token')
+    this.getAllProjects()
   }
+  
+  getAllProjects(){
+    let headers = new HttpHeaders({ 'authorization':this.token});
+   this.http.get("http://localhost:3000/user/profile",{headers:headers}).subscribe(res =>{
 
+     this.data = res
+     this.projects = this.data[0].project
+     console.log(this.projects)
+    //  for(let i = 0; i < this.projects.length; i){
+    //   this.projects[i] = 
+    //  }
+    //  this.getDetail("61d082c7f63072bebdb72aa3")
+   })
+ }
+ delete(_id:any){
+  this.http.delete("http://localhost:3000/project/delete/"+_id).subscribe(res =>{
+    console.log(res)
+    this.ngOnInit()
+  })
+  console.log(_id)
+ }
+ getDetail(_id:any){
+  this.http.get("http://localhost:3000/detail/details/").subscribe(res =>{
+    console.log(res)
+  })
+}
 }
